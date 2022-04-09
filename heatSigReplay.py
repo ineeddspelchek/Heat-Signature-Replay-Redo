c9a738b2d3aea63a3bba8207971048c61c078e44
ineeddspelchek <74257157+ineeddspelchek@users.noreply.github.com>
2022-04-09 13:11:50 -0400

    Update heatSigReplay.py

--- heatSigReplay.py
+++ heatSigReplay.py
@@ -21,6 +21,7 @@ from moviepy.editor import * #lets you edit videos
 recordToggle = False #True if you only want one key to toggle recording; False if you want separate record and stop record buttons
 recordKey1 = keyboard.KeyCode.from_char('g') #record button or toggle
 recordKey2 = keyboard.KeyCode.from_char('h') #stop record button
+exitKey = keyboard.KeyCode.from_char(';') #key to stop program
 #   *example keybinds: keyboard.KeyCode.from_char('a'), keyboard.Key.space, keyboard.Key.alt_l, keyboard.Key.ctrl_r
 keepFastMo = True #when true, doesn't slow down fast mo (if false, those segments go down to 5 fps)
 ###################################
@@ -49,104 +50,95 @@ process2.open("Heat_Signature.exe")
 
 ######################################################################################################################
 
-def record(key): #handle record presses
+recorder = mss.mss() #screenshot taker set up
+recording = False 
+fps = 1
+
+def pressHandler(key): #handle presses
     global recordToggle, recording
+    
+    if(key == exitKey): #if exit key pressed, exit
+        os._exit(0)
     
     if(recordToggle): #if set to toggle
         if(key == recordKey1): #if record toggle pressed
             recording = not recording
             if(recording):
+                recordTimer = threading.Timer(0, record) #set up recording
+                recordTimer.start() #start recording   
                 print("RECORDING")
             else:
                 print("NOT RECORDING")
     else: #if not toggling
-        if(key == recordKey1): #if record key pressed
+        if(key == recordKey1 and not recording): #if record key pressed
             recording = True
+            recordTimer = threading.Timer(0, record) #set up recording
+            recordTimer.start() #start recording  
             print("RECORDING")
-        elif(key == recordKey2): #if stop record key pressed
+        elif(key == recordKey2 and recording): #if stop record key pressed
             recording = False
             print("NOT RECORDING")
-
-
-recording = False 
-listener = keyboard.Listener(on_press=record) #set up keyboard listener
-listener.start() #starts keyboard listener
-recorder = mss.mss() #screenshot taker set up
-
-def main():
-    global recording
-    
-    recording = False
-
-    while(True): #while code runs
-        #reset/set values
-        readyToEdit = False #if raw footage is ready to put together and edit
-        baseTime = time.time() #time recording began
-        currFrame = 0 #current frame
-        shots = [] #list of shots from recorder
-        prevSpeed = -1 #last recorded speed
-        currSpeed = c_double.from_buffer(process2.read_memory(my_pointer, buffer_size=8)).value #current speed; also adapted from the 3 youtube videos
-        times = [] #entries consisting of [timescale change start time, new timescale]
-        
-        state = 0 #0-Paused, 1-Slow, 2-Normal, 3-Fast
-        if(currSpeed == 0): #if starting paused
-            state = 0
-            times.append([time.time()-baseTime, 0])
-        elif(currSpeed < .6): #if starting slow
-            state = 1
-            times.append([time.time()-baseTime, .2])
-        elif(currSpeed >= .6 and (currSpeed <= 1 or keepFastMo)): #if starting normal (or fast, but ignoring it)
-            state = 2
-            times.append([time.time()-baseTime, 1])
-        else: #if starting fast (and not ignoring it)
-            state = 3
-            times.append([time.time()-baseTime, 6])
-        
-        while(recording):
-            if(time.time() - baseTime >= currFrame/30): #if ready for next screenshot (1/30 of a second has passed since last one)
-                prevSpeed = currSpeed #last current speed is new previous speed
-                currSpeed = c_double.from_buffer(process2.read_memory(my_pointer, buffer_size=8)).value #new current speed accessed; also adapted from the 3 youtube videos
-                if(currSpeed != prevSpeed): #if speed has changed
-                    if(currSpeed == 0 and state != 0): #if now paused but not yet considered paused
-                        state = 0
-                        times.append([time.time()-baseTime+generalOffset, 0])
-                    elif(currSpeed < .6 and state != 1): #if now slow but not yet considered slow
-                        state = 1
-                        times.append([time.time()-baseTime+generalOffset, .2])
-                    elif(currSpeed >= .6 and currSpeed <= 1 and state != 2): #if now normal but not yet considered normal
-                        state = 2
-                        times.append([time.time()-baseTime+generalOffset, 1])
-                    elif(currSpeed > 1 and state != 3 and not keepFastMo): #if now fast but not yet considered fast (and not ignoring fast)
-                        state = 3
-                        times.append([time.time()-baseTime+generalOffset, 6])
-                        
-                shots.append(pyautogui.screenshot()) #take a screenshot and store it
-                currFrame += 1 #increment current frame
-            readyToEdit = True
             
-        if(readyToEdit): #if raw footage ready to put together and edit
-            print(times) #print timestamp entries
-            editTimer = threading.Timer(0, edit, args=[times, shots]) #set up edit
-            editTimer.start() #start edit
-            
-            
-
-def edit(times, shots): #create and edit raw footage from speed change timestamps and screenshots
+def record():
+    #reset/set values
     now = datetime.now() #current time
     timeStr = str(now.month) + "-" + str(now.day) + "-" + str(now.year) + "_" + str(now.hour) + "," + str(now.minute) + "," + str(now.second) #file identifier
-    
     #from https://www.thepythoncode.com/article/make-screen-recorder-python 
-    raw = cv2.VideoWriter(timeStr+"_raw.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 30, tuple(pyautogui.size()))
-    
-    for i in range(0, len(shots)):
-        frame = np.array(shots[i])
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        raw.write(frame)
-    raw.release()
+    raw = cv2.VideoWriter(timeStr+"_raw.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, tuple(pyautogui.size()))
     #######################################################################
     
+    baseTime = time.time() #time recording began
+    currFrame = 0 #current frame
+    prevSpeed = -1 #last recorded speed
+    currSpeed = c_double.from_buffer(process2.read_memory(my_pointer, buffer_size=8)).value #current speed; also adapted from the 3 youtube videos
+    times = [] #entries consisting of [timescale change start time, new timescale]
     
-    inVid = VideoFileClip(timeStr+"_raw.mp4") #raw input clip
+    state = 0 #0-Paused, 1-Slow, 2-Normal, 3-Fast
+    if(currSpeed == 0): #if starting paused
+        state = 0
+        times.append([time.time()-baseTime, 0])
+    elif(currSpeed < .6): #if starting slow
+        state = 1
+        times.append([time.time()-baseTime, .2])
+    elif(currSpeed >= .6 and (currSpeed <= 1 or keepFastMo)): #if starting normal (or fast, but ignoring it)
+        state = 2
+        times.append([time.time()-baseTime, 1])
+    else: #if starting fast (and not ignoring it)
+        state = 3
+        times.append([time.time()-baseTime, 6])
+
+    while(recording):
+        if(time.time() - baseTime >= currFrame/fps): #if ready for next screenshot (1/fps of a second has passed since last one)
+            prevSpeed = currSpeed #last current speed is new previous speed
+            currSpeed = c_double.from_buffer(process2.read_memory(my_pointer, buffer_size=8)).value #new current speed accessed; also adapted from the 3 youtube videos
+            if(currSpeed != prevSpeed): #if speed has changed
+                if(currSpeed == 0 and state != 0): #if now paused but not yet considered paused
+                    state = 0
+                    times.append([time.time()-baseTime+generalOffset, 0])
+                elif(currSpeed < .6 and state != 1): #if now slow but not yet considered slow
+                    state = 1
+                    times.append([time.time()-baseTime+generalOffset, .2])
+                elif(currSpeed >= .6 and currSpeed <= 1 and state != 2): #if now normal but not yet considered normal
+                    state = 2
+                    times.append([time.time()-baseTime+generalOffset, 1])
+                elif(currSpeed > 1 and state != 3 and not keepFastMo): #if now fast but not yet considered fast (and not ignoring fast)
+                    state = 3
+                    times.append([time.time()-baseTime+generalOffset, 6])
+
+            frame = np.array(pyautogui.screenshot()) #take a screenshot and store it
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            raw.write(frame)
+            currFrame += 1 #increment current frame
+    raw.release()
+    print(times) #print timestamp entries
+    print(fps*(time.time()-baseTime)/currFrame)
+    editTimer = threading.Timer(0, edit, args=[timeStr+"_raw.mp4", times]) #set up edit
+    editTimer.start() #start edit
+        
+            
+
+def edit(rawFileName, times): #create and edit raw footage from recording name and speed change timestamps 
+    inVid = VideoFileClip(rawFileName) #raw input clip
     
     clips = [] #list of clips to be combined
     for i in range(0, len(times)-1): #for all speed changes excluding the last
@@ -172,7 +164,10 @@ def edit(times, shots): #create and edit raw footage from speed change timestamp
         clips.append(inVid)
 
     outVid = concatenate_videoclips(clips) #combine clips into one
-    outVid.write_videofile(timeStr+"_out.mp4", fps=30) #output final mp4
+    outVid.write_videofile(rawFileName[:-8]+"_out.mp4", fps=fps) #output final mp4
 
 
-main() #run main
+listener = keyboard.Listener(on_press=pressHandler) #set up keyboard listener
+listener.start() #starts keyboard listener
+while(True): #program exited with exit key
+    pass
